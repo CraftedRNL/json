fc4af17ee0846b7782ebdaef08c3eab86baa7a98
CraftedRNL <126Rliu@frhsd.com>
2026-01-16 08:34:28 -0500

    Foreach

--- java.js
+++ java.js
@@ -6,7 +6,33 @@
             // document.getElementById("player-info").innerHTML = player.fullName + ": " + player.position;
             if(xhr.status === 200){
                 const data = JSON.parse(xhr.responseText);
+                const players = data.players;
+                const container = document.getElementById("cards");
                 
+
+
+                
+
+               
+                
+                players.forEach(player => {
+                    const card = document.createElement('div');
+                    let statsHtml ="";
+                    let teamsHtml ="";
+                    let awardsHtml ="";
+
+                    for (let i = 0; i < player.teams.length; i++) {
+                        
+                    
+                }
+
+                });
+
+                
+
+             
+                
+                    console.log(players);
             }
         }
 
